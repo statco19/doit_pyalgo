f04fff29494aa1b1d5aa1b74b264ef2f51e68761
Stephen <statco19@gmail.com>
2020-09-26 17:51:31 +0900

    Add files via upload

---
+++ CastingEx1.java
@@ -0,0 +1,16 @@
+
+public class CastingEx1{
+
+    public static void main(String args[]){
+
+       double d = 85.4;
+		int score = (int)d;
+		int c = 10;
+		
+		System.out.println("score="+score);
+		System.out.println("d="+d);
+	}
+}
+
+
+
