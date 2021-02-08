55dfab8d0b62f73f59268cbef7ed099a21b53817
Stephen <statco19@gmail.com>
2021-02-08 15:58:34 +0900

    created more methods and classes
    2/8

--- ch8/double_list.py
+++ ch8/double_list.py
@@ -1,6 +1,6 @@
 class Node (object):
 	
-	def __init__(self, data, prev, next): # prev, next are Node class
+	def __init__(self, data = None, prev = None, next = None): # prev, next are Node class
 		self.data = data
 		self.prev = prev or self
 		self.next = next or self
@@ -15,7 +15,7 @@ class DoubleLinkedList (object):
 		return self.no
 		
 	def is_empty(self):
-		return if self.head.next is self.head
+		return self.head.next is self.head
 		# since even an empty list has one dummy node(which has itself for its head and prev pointers),
 		# we cannot judge if the list is empty by whether the number of the list is 0 or not.
 		
@@ -79,11 +79,94 @@ class DoubleLinkedList (object):
 		"add a node at the first place."
 		
 		self.current = self.head
-		add(data)
+		self.add(data)
 	
 	def add_last(self, data):
 		"add a node at the last place."
 		
 		self.current = self.head.prev
 		add(data)
+	
+	def remove_current_node(self):
+		"remove the current node."
 		
+		if not self.is_empty():
+			self.current.prev.next = self.current.next
+			self.current.next.prev = self.current.prev
+			self.current = self.current.prev
+			self.no -= 1
+			if self.current is self.head:
+				self.current = self.head.next
+			
+	def remove(self, p):
+		"remove the node p."
+		
+		ptr = self.head.next
+		
+		while ptr is not self.head:
+			if ptr is p:
+				self.current = p
+				self.remove_current_node()
+				break
+			ptr = ptr.next
+			
+	def remove_first(self):
+		"remove the first node."
+		
+		self.current = self.head.next
+		self.remove_current_node()
+	
+	def remove_last(self):
+		"remove the last node."
+		
+		self.current = self.head.prev
+		self.remove_current_node()
+		
+	def clear(self):
+		"remove all"
+		
+		while not self.is_empty():
+			self.remove_first()
+		self.no = 0
+	
+	def __iter__(self):  # -> DoubleLinkedListIterator
+		"return an iterator."
+		return DoubleLinkedListIterator(self.head)
+		
+	def __reversed__(self):  # -> DoubleLinkedListReverseIterator
+		"return a reverse iterator."
+		return DoubleLinkedListReverseIterator(self.head)
+		
+class DoubleLinkedListIterator (object):
+	
+	def __init__(self, head):  # head: Node
+		self.head = head
+		self.current = head.next
+		
+	def __iter__(self):
+		return self
+		
+	def __next__(self):
+		if self.current is self.head:
+			raise StopIteration
+		else:
+			data = self.current.data
+			self.current = self.current.next
+			return data
+			
+class DoubleLinkedListReverseIterator (object):
+	
+	def __init__(self, head):  # head: Node
+		self.head = head
+		self.current = head.prev
+		
+	def __iter__(self):
+		return self
+		
+	def __next__(self):
+		if self.current is self.head:
+			raise StopIteration
+		else:
+			data = self.current.data
+			self.current = self.current.prev
+			return data
